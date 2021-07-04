ffb5fc2d9166c4280c0f92d4762e1e75da95020e
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2021-07-04 14:57:35 -0300

    visual: added styling to home screen

--- desafiogeosapiens/src/pages/Home/index.js
+++ desafiogeosapiens/src/pages/Home/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 import Header from '../../components/Header';
+import background from '../../assets/images/background.jpeg';
+
+import { Container, Image, Card } from './styles';
 
 function Home({ match }) {
   const { path } = match;
 
   return (
-    <Header routePath={path} />
+    <>
+      <Header routePath={path} />
+      <Container>
+        <Image src={background} />
+        <Card />
+      </Container>
+    </>
   );
 }
 
---
+++ desafiogeosapiens/src/pages/Home/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 350px;
+  object-fit: cover;
+  object-position: 0 70%;
+  border-radius: 0 0 8px 8px;
+`;
+
+export const Card = styled.div`
+  width: 60%;
+  min-height: 400px;
+  background-color: var(--color-white);
+  border-radius: 24px;
+  box-shadow: black 0px 5px 40px -30px;
+  margin-top: -150px;
+`;
