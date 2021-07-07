3d99147d6fdd6ca34896574fc739750a99f18ec6
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2021-07-07 09:58:43 -0300

    visual: created rating component

---
+++ desafiogeosapiens/src/components/Rating/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+import { Container, StarsContainer, Star } from './styles';
+
+function RatingComponent({label, id, onChange}) {
+  const [ value, setValue ] = useState(0);
+
+  const handleOnClick = (value) => {
+    setValue(value);
+    onChange(value, id)
+  }
+
+  const RenderStars = () => (
+    [...Array(5)].map((_, index) => <Star full={index < value} value={index+1} onClick={() => handleOnClick(index + 1)} />)
+  )
+
+  return (
+    <Container>
+      <label>{label}</label>
+      <StarsContainer>
+        <RenderStars />
+      </StarsContainer>
+    </Container>
+  );
+}
+
+export default RatingComponent;
