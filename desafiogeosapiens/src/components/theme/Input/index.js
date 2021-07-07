855f61071d54a20c25afa47d53cfd3524c1732f7
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2021-07-06 23:33:21 -0300

    visual: added select input in the component

--- desafiogeosapiens/src/components/theme/Input/index.js
+++ desafiogeosapiens/src/components/theme/Input/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-import { Container, InputContainer } from './styles';
+import { Container, InputContainer, SelectContainer, Option } from './styles';
 
-function Input({ id, label, type, onChange}) {
+function Input({ id, label, type, onChange, options}) {
   return (
     <Container key={id}>
       <label>{label}</label>
-      <InputContainer type={type} onChange={onChange} id={id} />
+      {type === 'select' ? (
+        <SelectContainer name={id} multiple onChange={onChange} id={id}>
+          {options.map(option => (
+            <Option value={option}>{option}</Option>
+          ))}
+        </SelectContainer>
+      ) : (
+        <InputContainer type={type} onChange={onChange} id={id} />
+      )}
     </Container>
   );
 }
--- desafiogeosapiens/src/components/theme/Input/styles.js
+++ desafiogeosapiens/src/components/theme/Input/styles.js
@@ -8,10 +8,24 @@ export const Container = styled.div`
 `
 
 export const InputContainer = styled.input`
-  padding: 8px 16px;
+  padding: 8px;
   border-radius: 8px;
   border: 1px solid var(--color-secondary);
   &:focus {
     border: 1px solid var(--color-primary);
   }
 `
+export const SelectContainer = styled.select`
+  border-radius: 8px;
+  border: 1px solid var(--color-secondary);
+  &:focus {
+    border: 1px solid var(--color-primary);
+  }
+`;
+
+export const Option = styled.option`
+  padding: 4px 8px;
+  &:hover {
+    background: var(--color-primary-light);
+  };
+`;
